57adeb42f4a2403dc91bc2d61e0a956c1c89d362
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:26:29 +0300

    Create PC PlayerController

---
+++ Assets/Scripts/CustomUI/PlayerController/PC/PlayerControllerPCPresenter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace CustomUI.PlayerController.PC
+{
+    public class PlayerControllerPCPresenter : IPlayerControllerPresenter
+    {
+        private Vector2 _inputVector;
+
+        public Vector2 GetInputVector()
+        {
+            _inputVector.x = Input.GetAxis("Horizontal");
+            _inputVector.y = Input.GetAxis("Vertical");
+
+            if (_inputVector.magnitude > 1)
+            {
+                _inputVector.Normalize();
+            }
+
+            return _inputVector;
+        }
+    }
+}
