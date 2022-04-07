f3473aff8c5dc731fccecb57b6d835967ac78d1c
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:26:43 +0300

    Create PlayerController Installer

---
+++ Assets/Scripts/CustomUI/PlayerController/PlayerControllerInstaller.cs
@@ -0,0 +1,50 @@
+﻿using CustomUI.PlayerController.Mobile;
+using CustomUI.PlayerController.PC;
+using UnityEngine;
+using Zenject;
+
+namespace CustomUI.PlayerController
+{
+    public sealed class PlayerControllerInstaller :
+        MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            if (Application.platform == RuntimePlatform.WindowsEditor ||
+                Application.platform == RuntimePlatform.WindowsPlayer)
+            {
+                InstallPCPresenter();
+            }
+            else
+            {
+                InstallMobilePresenter();
+            }
+        }
+
+
+        [SerializeField] private PlayerControllerMobileView mobileView;
+        [SerializeField] private PlayerControllerMobileSettings mobileSettings;
+
+        private void InstallMobilePresenter()
+        {
+            mobileView.Construct(mobileSettings);
+            BindMobilePresenter();
+        }
+
+        private void BindMobilePresenter()
+        {
+            Container
+                .Bind<IPlayerControllerPresenter>()
+                .FromInstance(new PlayerControllerMobilePresenter(mobileView))
+                .AsSingle();
+        }
+
+        private void InstallPCPresenter()
+        {
+            Container
+                .Bind<IPlayerControllerPresenter>()
+                .FromInstance(new PlayerControllerPCPresenter())
+                .AsSingle();
+        }
+    }
+}
