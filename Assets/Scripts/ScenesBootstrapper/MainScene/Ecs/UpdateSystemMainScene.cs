2281df95fb22531f775cc07da7068759df1a5602
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 20:02:26 +0300

    Create Update System

---
+++ Assets/Scripts/ScenesBootstrapper/MainScene/Ecs/UpdateSystemMainScene.cs
@@ -0,0 +1,65 @@
+﻿using CustomUI.PlayerController;
+using ECS.Systems;
+using Leopotam.Ecs;
+using UnityEngine;
+using Voody.UniLeo;
+using Zenject;
+
+namespace ScenesBootstrapper.MainScene.Ecs
+{
+    public class UpdateSystemMainScene : MonoBehaviour
+    {
+        private EcsWorld _world;
+        private EcsSystems _systems;
+
+        public void Construct(ref EcsWorld world)
+        {
+            _world = world;
+
+            _systems = new EcsSystems(_world);
+            _systems.ConvertScene();
+
+            AddInjections();
+            AddOneFrames();
+            AddSystems();
+
+            _systems.Init();
+        }
+
+        public void Run()
+        {
+            _systems.Run();
+        }
+        
+        
+        private void AddInjections()
+        {
+        }
+
+        private void AddOneFrames()
+        {
+        }
+
+        private void AddSystems()
+        {
+            _systems
+                // init
+                .Add(new LoadRandomMapSystem())
+
+                //Run
+                ;
+        }
+
+        private void OnDestroy()
+        {
+            DestroySystems();
+        }
+
+        private void DestroySystems()
+        {
+            if (_systems == null) return;
+            _systems.Destroy();
+            _systems = null;
+        }
+    }
+}
