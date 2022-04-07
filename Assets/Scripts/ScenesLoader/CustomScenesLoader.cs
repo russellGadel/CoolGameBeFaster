452a16b8fe872705fefd679a4e25374024ffbd07
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:13:26 +0300

    Create CustomScenesLoader
    Load scenes with using ScenesNaming
    Functionality: Load, LoadAsync, ExecuteSceneEvents

---
+++ Assets/Scripts/ScenesLoader/CustomScenesLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using ScenesBootstrapper;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ScenesLoader
+{
+    public sealed class CustomScenesLoader : ICustomScenesLoader
+    {
+        public IEnumerator LoadScene(ScenesNaming sceneNaming, ISceneBootstrapper currentSceneBootstrapper)
+        {
+            currentSceneBootstrapper.Exit();
+
+            SceneManager.LoadScene(sceneNaming.ToString());
+            Scene scene = SceneManager.GetSceneByName(sceneNaming.ToString());
+
+            yield return new WaitWhile(() => scene.isLoaded == false);
+        }
+
+        public IEnumerator LoadSceneAsync(ScenesNaming sceneNaming, ISceneBootstrapper currentSceneBootstrapper)
+        {
+            currentSceneBootstrapper.Exit();
+
+            SceneManager.LoadSceneAsync(sceneNaming.ToString());
+            Scene scene = SceneManager.GetSceneByName(sceneNaming.ToString());
+
+            yield return new WaitWhile(() => scene.isLoaded == false);
+        }
+
+        public bool IsLoaded(ScenesNaming sceneName)
+        {
+            return SceneManager.GetSceneByName(sceneName.ToString()).isLoaded;
+        }
+    }
+}
---
+++ Assets/Scripts/ScenesLoader/ICustomScenesLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using ScenesBootstrapper;
+
+namespace ScenesLoader
+{
+    public interface ICustomScenesLoader
+    {
+        IEnumerator LoadScene(ScenesNaming sceneNaming, ISceneBootstrapper currentSceneBootstrapper);
+        IEnumerator LoadSceneAsync(ScenesNaming sceneNaming, ISceneBootstrapper currentSceneBootstrapper);
+        bool IsLoaded(ScenesNaming sceneName);
+    }
+}
