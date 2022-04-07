c1369282afcb325fefd5b33af4bda7905ffe70d5
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:15:55 +0300

    Create LoadingSceneBootstrapperInstaller

---
+++ Assets/Scripts/ScenesBootstrapper/LoadingScene/LoadingSceneBootstrapper.cs
@@ -0,0 +1,36 @@
+﻿using Core.EventsExecutor;
+using ScenesBootstrapper.LoadingScene.Events;
+using ScenesLoader;
+using UnityEngine;
+using Zenject;
+
+namespace ScenesBootstrapper.LoadingScene
+{
+    public sealed class LoadingSceneBootstrapper : ISceneBootstrapper
+    {
+        //   private ILoadingWindowPresenter _loadingWindow;
+        [Inject] private readonly ICustomEventsExecutor _executor;
+        [Inject] private readonly ICustomScenesLoader _scenesLoader;
+
+        public void Enter()
+        {
+            //   _loadingWindow.Open();
+
+            Debug.Log("LoadingScene Enter");
+
+            AddEnterItems();
+
+            _executor.Execute();
+        }
+
+        public void Exit()
+        {
+        }
+
+        private void AddEnterItems()
+        {
+            _executor.Clear();
+            _executor.AddEvent(new LoadMainSceneEvent(_scenesLoader, this));
+        }
+    }
+}
