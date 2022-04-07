9c52987f2ffc3cb0a7e837ce21374267bba73669
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:14:49 +0300

    Create Game Bootstrapper

---
+++ Assets/Scripts/ScenesBootstrapper/GameBootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ScenesBootstrapper
+{
+    public class GameBootstrapper : MonoBehaviour
+    {
+        [Inject] private ISceneBootstrapper _bootstrapper;
+        
+        private void Awake()
+        {
+            _bootstrapper.Enter();
+        }
+    }
+}
