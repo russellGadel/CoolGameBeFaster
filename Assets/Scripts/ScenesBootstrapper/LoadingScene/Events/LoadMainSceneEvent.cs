d77de0f0eca0a347f143c47dd71b386be3242abe
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:18:47 +0300

    create LoadMainSceneEvents

---
+++ Assets/Scripts/ScenesBootstrapper/LoadingScene/Events/LoadMainSceneEvent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using Core.EventsExecutor;
+using ScenesLoader;
+
+namespace ScenesBootstrapper.LoadingScene.Events
+{
+    public sealed class LoadMainSceneEvent : ICustomEvent
+    {
+        private readonly ICustomScenesLoader _scenesLoader;
+        private readonly LoadingSceneBootstrapper _loadingSceneBootstrapper;
+        
+        public LoadMainSceneEvent(ICustomScenesLoader scenesLoader,
+            LoadingSceneBootstrapper loadingSceneBootstrapper)
+        {
+            _scenesLoader = scenesLoader;
+            _loadingSceneBootstrapper = loadingSceneBootstrapper;
+            
+            
+        }
+
+        public IEnumerator Execute()
+        {
+            yield return _scenesLoader.LoadSceneAsync(ScenesNaming.MainScene, _loadingSceneBootstrapper);
+        }
+    }
+}
