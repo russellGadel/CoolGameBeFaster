de0ab619eb5d18165b0a5a77146510429b8c67a1
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-04-07 11:16:31 +0300

    Create MainSceneBootsrapper

---
+++ Assets/Scripts/ScenesBootstrapper/MainScene/MainSceneBootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Core.BootstrapExecutor;
+using Zenject;
+
+namespace ScenesBootstrapper.MainScene
+{
+    public sealed class MainSceneBootstrapper : ISceneBootstrapper
+    {
+        public void Enter()
+        {
+            AddEnterItems();
+            _executor.Execute();
+        }
+
+        public void Exit()
+        {
+        }
+
+        [Inject] private IBootstrapExecutor _executor;
+        [Inject] private UIBootstrapperMainScene _uiBootstrapperMainScene;
+        [Inject] private EscBootstrapperMainScene _escBootstrapperMainScene;
+
+        private void AddEnterItems()
+        {
+            _executor.Clear();
+            _executor.Add(_uiBootstrapperMainScene);
+            _executor.Add(_escBootstrapperMainScene);
+        }
+    }
+}
---
+++ Assets/Scripts/ScenesBootstrapper/MainScene/MainSceneBootstrapperInstaller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace ScenesBootstrapper.MainScene
+{
+    public sealed class MainSceneBootstrapperInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            BindMainSceneBootstrapper();
+            BindEscBootstrapper();
+            BindUIInstaller();
+        }
+
+
+        private void BindMainSceneBootstrapper()
+        {
+            Container
+                .Bind<ISceneBootstrapper>()
+                .To<MainSceneBootstrapper>()
+                .AsSingle();
+        }
+
+
+        [SerializeField] private EscBootstrapperMainScene _escBootstrapper;
+
+        private void BindEscBootstrapper()
+        {
+            Container
+                .Bind<EscBootstrapperMainScene>()
+                .FromInstance(_escBootstrapper)
+                .AsSingle();
+        }
+
+        private void BindUIInstaller()
+        {
+            Container
+                .Bind<UIBootstrapperMainScene>()
+                .AsSingle();
+        }
+    }
+}
