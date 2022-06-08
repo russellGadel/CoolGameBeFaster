1654a4a9951ecc19bf02b1d82753ff3ad78fb54f
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-06-08 22:38:28 +0300

    Refactored ActivateObjectsSystem.cs

--- Assets/Scripts/ECS/Systems/Events/ObjectsActivitySystem/ActivateObjectsSystem.cs
+++ Assets/Scripts/ECS/Systems/Events/ObjectsActivitySystem/ActivateObjectsSystem.cs
@@ -1,5 +1,4 @@
-﻿using ECS.Components;
-using ECS.Components.GameObjectComponent;
+﻿using ECS.Components.GameObjectComponent;
 using ECS.Events;
 using ECS.Tags;
 using Leopotam.Ecs;
@@ -12,16 +11,16 @@ namespace ECS.Systems.Events.ObjectsActivitySystem
 
         public void Run()
         {
-            foreach (var idx in _objects)
+            foreach (int idx in _objects)
             {
                 ref EcsEntity entity = ref _objects.GetEntity(idx);
 
                 entity.Del<InactiveObjectTag>();
                 entity.Get<ActiveObjectTag>();
-                
+
                 ref GameObjectComponent gameObjectComponent = ref _objects.Get2(idx);
                 gameObjectComponent.gameObject.SetActive(true);
-                
+
                 entity.Del<ActivateObjectEvent>();
             }
         }
