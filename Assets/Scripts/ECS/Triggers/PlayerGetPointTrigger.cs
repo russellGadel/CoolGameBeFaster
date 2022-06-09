ab4ee87049a0ca821ec26765c3f6f46a22ed7d96
russellGadel <88060291+russellGadel@users.noreply.github.com>
2022-06-09 16:33:44 +0300

    Refactored  PlayerGetPointTrigger.cs

--- Assets/Scripts/ECS/Triggers/PlayerGetPointTrigger.cs
+++ Assets/Scripts/ECS/Triggers/PlayerGetPointTrigger.cs
@@ -8,15 +8,16 @@ namespace ECS.Triggers
 {
     public class PlayerGetPointTrigger : MonoBehaviour
     {
+        [SerializeField] private MonoEntity _playerMonoEntity;
+
         private void OnTriggerEnter2D(Collider2D col)
         {
             if (col.CompareTag(UnityTags.Point.ToString()))
             {
+                _playerMonoEntity.Entity.Replace(new PlayerGetPointEvent());
+
                 MonoEntity pointMonoEntity = col.gameObject.GetComponent<MonoEntity>();
                 pointMonoEntity.Entity.Replace(new DeactivateObjectEvent());
-
-                MonoEntity playerMonoEntity = gameObject.GetComponent<MonoEntity>();
-                playerMonoEntity.Entity.Replace(new PlayerGetPointEvent());
             }
         }
     }
